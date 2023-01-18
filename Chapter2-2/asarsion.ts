6552d2ce102b51d40cd00ed035100bee25caeef9
hagiwa901 <kedama4156991@gmail.com>
2023-01-18 23:52:49 +0900

    add chapter2

---
+++ Chapter2-2/asarsion.ts
@@ -0,0 +1,5 @@
+const myCanvas = document.getElementById('main_canvas')
+console.log(myCanvas.width)
+
+const mCanvas = document.getElementById('main_canvas') as HTMLCanvasElement
+console.log(mCanvas.width)
