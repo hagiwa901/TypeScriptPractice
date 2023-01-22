e15ae14471f4a0b4d000eb7f27e7e17e67cedfc7
hagiwa901 <kedama4156991@gmail.com>
2023-01-23 00:02:06 +0900

    add

--- Chapter2-2/class.ts
+++ Chapter2-2/class.ts
@@ -6,4 +6,29 @@ class Point{
         this.x = x
         this.y = y
     }
+
+    moveX(n: number): void{
+        this.x += n
+    }
+
+    moveY(n: number): void{
+        this.y += n
+    }
 }
+
+class Point3D extends Point{
+    z: number;
+
+    constructor(x: number=0, y:number=0, z: number=0){
+        super(x, y)
+        this.z = z
+    }
+
+    moveZ(n: number): void{
+        this.z += n
+    }
+}
+
+const point = new Point()
+point.moveX(10)
+console.log('${point.x}, ${point.y}')
