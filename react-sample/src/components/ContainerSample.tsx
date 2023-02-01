d36b8e741ca00a22f08a854806c7ace75a4e8089
hagiwa901 <kedama4156991@gmail.com>
2023-02-01 23:47:45 +0900

    add

--- react-sample/src/components/ContainerSample.tsx
+++ react-sample/src/components/ContainerSample.tsx
@@ -1,5 +1,12 @@
+import React from 'react'
+
+type ContainerProps = {
+    title: string
+    children: React.ReactNode
+}
+
 // Containerは赤背景のボックスの中にタイトルと小要素を表示
-const Container=(props: {title: string; children: React.ReactElement }) => {
+const Container=(props: ContainerProps): JSX.Element => {
     const { title, children } = props
 
     return (
@@ -11,7 +18,7 @@ const Container=(props: {title: string; children: React.ReactElement }) => {
     )
 }
 
-const Parent = () => {
+const Parent = (): JSX.Element => {
     return (
         <Container title="Hello">
             <p>ここの部分が背景色で囲まれる</p>
---
+++ react-sample/src/components/ContextSample.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+
+// Titleを渡すためのContextを作成
+const TitleContext = React.createContext('')
+
+// Titleコンポーネントの中でContextの値を参照
+const Title = () => {
+    // Consumerを使って、Contextの値を参照
+    return (
+        <TitleContext.Consumer>
+            { /* Consumer直下に関数をおいて、Contextの値を参照 */ }
+            {(title) => {
+                return <h1>{title}</h1>
+            }}
+        </TitleContext.Consumer>
+    )
+}
+
+const Header = () => {
+    return (
+        <div>
+            {/* HeaderからTitleへは何もデータを渡さない */}
+            <Title />
+        </div>
+    )
+}
+
+// Pageコンポーネントの中でContextに値を渡す
+const Page = () => {
+    const title = 'React Book'
+
+    // Providerを使いContextに値をセット
+    // Provider以下のコンポーネントから値を参照
+    return (
+        <TitleContext.Provider value={title}>
+            <Header />
+        </TitleContext.Provider>
+    )
+}
+
+export default Page
