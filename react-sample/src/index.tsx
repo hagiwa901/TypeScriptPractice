ca71416cbbb85d3b4ee86489d784ac90cfd83fe0
hagiwa901 <kedama4156991@gmail.com>
2023-02-02 23:53:26 +0900

    add

--- react-sample/src/index.tsx
+++ react-sample/src/index.tsx
@@ -3,14 +3,23 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 //import App from './App';
 import Hello from './components/Hello';
+import Parent from './components/ContainerSample'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+/*
 root.render(
   <React.StrictMode>
     <Hello />
+  </React.StrictMode>
+);
+*/
+
+root.render(
+  <React.StrictMode>
+    <Parent />
   </React.StrictMode>
 );
 
